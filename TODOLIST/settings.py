0281735dafc92767d3c22b178a93ef49335c2a88
George <georgepavlej@gmail.com>
2022-12-16 12:43:29 +0000

    Add views, urls, models and create templates

--- TODOLIST/settings.py
+++ TODOLIST/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = 'TODOLIST.wsgi.application'
 
